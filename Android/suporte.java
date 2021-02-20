dc6a66bd4e2121f69c833b7d6eb4b2e121c35628
anonymousskt <77140403+anonymousskt@users.noreply.github.com>
2021-02-20 17:41:21 -0300

    Add files via upload

---
+++ Android/suporte.java
@@ -0,0 +1,9 @@
+/*
+ * Classname             (sadzinbot3.0)
+ *
+ * Version information   (3.0)
+ *
+ * Date                  (06/12,15:35)
+ *
+ * author                (Rauny Yt)
+ * Copyright notice      (bot com suporte a Android/Java)
